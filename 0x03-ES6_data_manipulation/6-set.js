7ce03fb868cb110a6edcc715b3c39759ccb32fd2
Soid <dorrismutahi@gmail.com>
2024-07-02 21:40:42 +0300

    manipulation in js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Dorris Mutahi <https://github.com/Soid23>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+    return new Set(array);
+  }
